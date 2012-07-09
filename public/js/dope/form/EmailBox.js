44079fbffd39fd18cb9b24f6ac2a0d6b075249c8
Jonathan M. (Doghouse Media) <jonathan@dhmedia.com.au>
2012-07-09 18:11:13 +0800

    EMailBox

--- public/js/dope/form/EmailBox.js
+++ public/js/dope/form/EmailBox.js
@@ -2,5 +2,69 @@ dojo.provide('dope.form.EmailBox');
 dojo.require('dijit.form.TextBox');
 
 dojo.declare('dope.form.EmailBox', dijit.form.TextBox, {
+	contacts: [],
 	
+	startup: function() {
+		this.inherited(arguments);
+		dojo.addClass(this.domNode, 'dopeEmailBox');
+		this.reset();
+		this.addContact('jonathan@dhmedia.com.au');
+		dojo.connect(this, 'onKeyUp', this.parseValueForContacts.bind(this));
+	},
+	onChange: function(newValue) {
+		console.log(this.get('value'), newValue);
+		this.parseValueForContacts(true);
+	},
+	parseValueForContacts: function(useLastPart) {
+		var values = this.get('value').split(/[,\s]/);
+		//this.clearContacts();
+		dojo.forEach(values, this.addContact.bind(this));
+	},
+	addContact: function(value) {
+		this.contacts.push(new dope.form.EmailBox.Contact({
+			'value': value,
+			'emailBox': this
+		}));
+	},
+	removeContact: function(contact) {
+		alert('Remove');
+	},
+	clearContacts: function() {
+		dojo.forEach(this.contacts, dojo.destroy);
+		dojo.empty(this.contactContainerNode);
+		this.contacts = [];
+	},
+	reset: function() {
+		if (! this.contactContainerNode) {
+			this.contactContainerNode = dojo.create('div', {
+				className: 'dopeEmailBoxContactContainer'
+			});
+			dojo.place(this.contactContainerNode, this.domNode, 'first');
+		}
+		
+		this.clearContacts();
+	}
+});
+
+dojo.declare('dope.form.EmailBox.Contact', [dijit._Widget, dijit._Templated], {
+	value: '',
+	emailBox: null,
+	baseClass: 'dopeEmailBoxContact',
+	
+	templateString: '<div>'
+		+ '<span data-dojo-attach-point="emailNode"></span>'
+		+ '<span data-dojo-attach-point="removeNode" class="dopeEmailBoxContactRemove">x</span>'
+		+ '</div>',
+
+	postCreate: function() {
+		this.inherited(arguments);
+		this.emailNode.innerHTML = this.get('value');
+		dojo.connect(this.removeNode, 'onclick', this.remove.bind(this));
+		
+		console.log(this, this.domNode, this.emailBox, this.emailBox.contactContainerNode);
+		dojo.place(this.domNode, this.emailBox.contactContainerNode, 'last');
+	},
+	remove: function() {
+		this.emailBox.removeContact(this);
+	}
 });
