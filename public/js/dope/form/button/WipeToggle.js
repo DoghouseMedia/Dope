b1f20a78c845e1e4cd3ab26adc7c6c5097457efb
Jonathan Melnick <jonathan@dhmedia.com.au>
2013-06-20 17:22:17 +0800

    Improve WipeToggle

--- public/js/dope/form/button/WipeToggle.js
+++ public/js/dope/form/button/WipeToggle.js
@@ -4,20 +4,43 @@ dojo.require('dope.form.Button');
 dojo.declare('dope.form.button.WipeToggle', dope.form.Button, {
 	href: '',
 	selector: '',
+	autoHide: false,
+	
+	postCreate: function() {
+		this.inherited(arguments);
+		
+		this.node = dojo.query(this.selector, this.getPane().domNode)[0];
+		
+		if (this.autoHide) {
+			dojo.style(this.node, "display", "none");
+			dojo.removeClass(this.node, 'visible');
+		}
+	},
+	
+	_hide: function() {
+		dojo.fx.wipeOut({'node': this.node}).play();
+		dojo.removeClass(this.node, 'visible');
+	},
+	
+	_show: function() {
+		dojo.fx.wipeIn({'node': this.node}).play();
+		dojo.addClass(this.node, 'visible');
+	},
+	
+	_isVisible: function() {
+		return dojo.hasClass(this.node, 'visible');
+	},
 	
 	onClick: function(e) {
 		this.inherited(arguments);
 		
-		var node = dojo.query(this.selector, this.getPane().domNode)[0];
-			
-		if (dojo.hasClass(node, 'visible')) {
-			dojo.fx.wipeOut({'node': node}).play();
-		} else {
-			dojo.fx.wipeIn({'node': node}).play();
-		}
-			
-		dojo.toggleClass(node, 'visible');
-		
 		if (e) dojo.stopEvent(e);
+		
+		if (this._isVisible()) {
+			this._hide();
+		}
+		else {
+			this._show();
+		}
 	}
 });
