a26fc6cdb8836fdec4ab9a06613f57dac6f77f2b
Jonathan Melnick <jonathan@dhmedia.com.au>
2013-10-04 08:59:02 +0800

    Fix locale to also work when compiled

--- public/js/dope/date/locale.js
+++ public/js/dope/date/locale.js
@@ -5,11 +5,24 @@ dojo.declare('dope.date.locale', null);
 
 dope.date.locale = dojo.date.locale;
 
-var _bundle = dojo.date.locale._getGregorianBundle();
-_bundle['dateFormat-iso'] = 'yyyy-MM-dd';
-_bundle['timeFormat-iso'] = 'hh:mm:ss';
-_bundle['dateTimeFormat-iso'] = '{1}T{0}';
-
-dojo.date.locale._getGregorianBundle = function() {	
-	return _bundle;
-};
+if (window.casper) {
+	var _bundle = dojo.date.locale._getGregorianBundle();
+	_bundle['dateFormat-iso'] = 'yyyy-MM-dd';
+	_bundle['timeFormat-iso'] = 'hh:mm:ss';
+	_bundle['dateTimeFormat-iso'] = '{1}T{0}';
+	
+	dojo.date.locale._getGregorianBundle = function() {	
+		return _bundle;
+	};
+}
+else {
+	var _parentGetGregorianBundle = dojo.date.locale._getGregorianBundle.bind({});
+	
+	dojo.date.locale._getGregorianBundle = function() {
+		var bundle = _parentGetGregorianBundle.call(arguments);
+		bundle['dateFormat-iso'] = 'yyyy-MM-dd';
+		bundle['timeFormat-iso'] = 'hh:mm:ss'
+		bundle['dateTimeFormat-iso'] = '{1}T{0}';
+		return bundle;
+	}
+}
