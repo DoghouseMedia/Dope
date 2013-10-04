2da5614bac287c13b1375f1376bf4c4996d7d8c5
Jonathan Melnick <jonathan@dhmedia.com.au>
2013-10-04 08:18:56 +0800

    Modified dope locale to play nice with CasperJS

--- public/js/dope/date/locale.js
+++ public/js/dope/date/locale.js
@@ -5,12 +5,11 @@ dojo.declare('dope.date.locale', null);
 
 dope.date.locale = dojo.date.locale;
 
-var _parentGetGregorianBundle = dojo.date.locale._getGregorianBundle.bind({});
+var _bundle = dojo.date.locale._getGregorianBundle();
+_bundle['dateFormat-iso'] = 'yyyy-MM-dd';
+_bundle['timeFormat-iso'] = 'hh:mm:ss';
+_bundle['dateTimeFormat-iso'] = '{1}T{0}';
 
-dojo.date.locale._getGregorianBundle = function() {
-	var bundle = _parentGetGregorianBundle.call(arguments);
-	bundle['dateFormat-iso'] = 'yyyy-MM-dd';
-	bundle['timeFormat-iso'] = 'hh:mm:ss'
-	bundle['dateTimeFormat-iso'] = '{1}T{0}';
-	return bundle;
+dojo.date.locale._getGregorianBundle = function() {	
+	return _bundle;
 };
