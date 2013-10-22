64a35a1fe1190057734e42ae452350c5c53f616e
Jonathan Melnick <jonathan@dhmedia.com.au>
2013-10-22 09:50:18 +0800

    Validation in TextArea

---
+++ lib/Dope/View/Helper/Textarea.php
@@ -0,0 +1,16 @@
+<?php
+
+class Dope_View_Helper_Textarea extends Zend_Dojo_View_Helper_Textarea
+{
+	/**
+	 * Dijit being used
+	 * @var string
+	 */
+	protected $_dijit  = 'dope.form.Textarea';
+	
+	/**
+	 * Dojo module to use
+	 * @var string
+	 */
+	protected $_module = 'dope.form.Textarea';
+}
