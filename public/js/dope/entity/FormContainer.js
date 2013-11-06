8fdb58f66c59c074d8e6bea15f571fb9183087fd
Jonathan Melnick <jonathan@dhmedia.com.au>
2013-11-06 13:36:35 +0800

    Fix form inheritance issue with FormContainer

--- public/js/dope/entity/FormContainer.js
+++ public/js/dope/entity/FormContainer.js
@@ -2,6 +2,6 @@ dojo.provide('dope.entity.FormContainer');
 dojo.require('dope.entity.Form');
 dojo.require('dope.layout.BorderContainer');
 
-dojo.declare('dope.entity.FormContainer', [dope.entity.Form, dope.layout.BorderContainer], {
+dojo.declare('dope.entity.FormContainer', [dope.layout.BorderContainer, dope.entity.Form], {
 	baseClass: 'dopeForm'
 });
