abdb8e924daf1b68f3b76efdec3ce17de6577b84
Jonathan Melnick <jonathan@dhmedia.com.au>
2013-11-08 13:43:03 +0800

    Textarea validation should accept line returns

--- public/js/dope/form/Textarea.js
+++ public/js/dope/form/Textarea.js
@@ -3,6 +3,8 @@ dojo.require("dijit.form.SimpleTextarea");
 dojo.require("dijit.form.ValidationTextBox");
 
 dojo.declare('dope.form.Textarea', [dijit.form.SimpleTextarea, dijit.form.ValidationTextBox], {
+  regExp: "^(.*\n?)*$",
+  
   templateString: "<textarea ${!nameAttrSetting} data-dojo-attach-point='focusNode,containerNode,textbox' autocomplete='off'></textarea>",
   
   constructor: function() {
