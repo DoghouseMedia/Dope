dba590ccff00f03c0082ae5fb18e59516461164e
Jonathan Melnick <jonathan@dhmedia.com.au>
2014-01-14 09:35:27 +0800

    Add Rest Token Auth

---
+++ lib/Dope/Controller/Plugin/RestTokenAuth.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dope\Controller\Plugin;
+
+use Dope\Auth\Service as Auth;
+
+class RestTokenAuth extends \Zend_Controller_Plugin_Abstract
+{
+	/**
+	 * Before dispatching, digest raw request body and set params
+	 *
+	 * @param \Zend_Controller_Request_Abstract $request
+	 */
+	public function preDispatch(\Zend_Controller_Request_Abstract $request)
+	{
+		if (!$request instanceof \Zend_Controller_Request_Http) {
+			return;
+		}
+
+        $this->getResponse()->setHeader('Access-Control-Allow-Origin', '*');
+        $this->getResponse()->setHeader('Access-Control-Allow-Headers', 'Dope-Rest-Token,Range');
+
+        if ('OPTIONS' == strtoupper($request->getMethod())) {
+            $this->getResponse()->sendHeaders();
+            die();
+        }
+
+        $token = $request->getHeader('Dope-Rest-Token');
+        if (!$token OR Auth::hasUser()) {
+            return;
+        }
+	
+        $user = \Dope\Doctrine::getRepository(Auth::getUserEntityClass())->findOneBy(array(
+            'token' => $token
+        ));
+
+        if (! $user) {
+            return;
+        }
+
+        Auth::setUser($user);
+	}
+}
